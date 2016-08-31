057af1c5871baf864f054caffbe6d53f132a9599
Andrew Chalkley <andrew@chalkley.org>
2016-08-31 14:34:30 -0700

    Experimental first commit

---
+++ example.sh
@@ -0,0 +1,6 @@
+echo "Waiting for tag or device...
+on_name_lost(): :org.neardal
+Tag found
+ISO14443A ATQA:         4400
+ISO14443A SAK:  00
+ISO14443A UID:  047869FAB24981";
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const config = require('./config.json');
+
+
+let lastId = "";
+
+function findNFC(callback) {
+    const spawn = require('child_process').spawn;
+    const nfc = spawn(config.Command);
+    let stdout = "";
+    nfc.stdout.on('data', (data) => {
+        stdout += data;
+    } );
+
+    nfc.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    nfc.on('close', (code) => {
+        if(code === 0) {
+            callback(null, stdout.match(/UID: (.*)+/g)[0].split(" ")[2]);
+        }
+    });
+}
+
+function playYTForUID(uid) {
+    console.log(config.Toy[uid]);
+}
+
+function handleNFC(err, uid) {
+    if(lastId !== uid) {
+        playYTForUID(uid);
+    }
+    lastId = uid;
+    findNFC(handleNFC);
+}
+
+findNFC(handleNFC);
